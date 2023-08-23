c005579af7454764a15c993db909b288a0520bd9
li <liyanlong@indemind.cn>
2023-08-23 15:05:54 +0800

    init

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#define MQTTCLIENT_QOS2 1
+
+#include <memory.h>
+#include "MQTTClient/src/MQTTClient.h"
+
+int main(int argc, char* argv[])
+{   
+	printf("MQTT connected\n");
+    
+    return 0;
+}
+
